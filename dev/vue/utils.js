1b72e7b929171c3dfbb6e00c36fc6cdfda5f93bd
Voltra <ludwig.guerin.98@gmx.fr>
2019-01-20 14:03:52 +0100

    Added lazy SFC loader function, mod e2e settings

---
+++ dev/vue/utils.js
@@ -0,0 +1,10 @@
+/**
+ * Create a lazy loaded component wrapper
+ * @param {string} path - The path to the SFC (relative to the components root)
+ * @returns {function(): (Promise<*>|*)}
+ */
+const lazyc = path => () => import(`@components/${path}.vue`);
+
+export {
+	lazyc,
+}
