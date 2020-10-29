bb9d3ed792f66cdca958c36ce6fd88a6a1c7cfa1
Voltra <ludwig.guerin.98@gmx.fr>
2020-10-29 13:40:40 +0100

    Remove unused qrcode dependency

--- dev/vue/components/QrCode.vue
+++ dev/vue/components/QrCode.vue
@@ -1,25 +1,15 @@
 <template>
-<!--	<canvas ref="canvas"></canvas>-->
 	<img :src="url" alt="QR code"/>
 </template>
 
 <script>
 	import VueTypes from "vue-types"
-	import QR from "qrcode"
 
 	export default {
 		name: "QrCode",
 		props: {
 			url: VueTypes.string.isRequired,
 		},
-		/*mounted(){
-			QR.toCanvas(this.$refs.canvas, this.url, {
-				errorCorrectionLevel: "H",
-			}, err => {
-				if(err)
-					console.error(err);
-			});
-		},*/
 	};
 </script>
 
