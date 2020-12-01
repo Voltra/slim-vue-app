e56547dd9a0ad509896ea82a78c6cfd937a32c6f
Voltra <ludwig.guerin.98@gmx.fr>
2020-12-01 22:52:17 +0100

    Add validation library and action

---
+++ dev/Actions/Validator.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Actions;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Rakit\Validation\Validation;
+
+class Validator extends Action
+{
+	public function make(ServerRequestInterface $req, array $rules, array $messages = []): Validation
+	{
+		$validator = new \Rakit\Validation\Validator($messages);
+		$data = $req->getParsedBody();
+		$data += $req->getQueryParams();
+		$data += $req->getUploadedFiles();
+		return $validator->make($data, $rules, $messages);
+	}
+}
